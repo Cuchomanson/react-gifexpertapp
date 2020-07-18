14f5fbf74aab42c56efe6167975d52d87d6c7a8f
Cuchomanson <Cucho_mdoz@hotmail.com>
2020-07-18 13:38:48 +0200

    docs agregado

---
+++ docs/precache-manifest.25762bcd87a8e0a33c23ba90d3778bb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ea641e14d7df257f093686e67dd521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798b10dcc694ce11720f",
+    "url": "/static/css/main.e5c2c8fc.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798b10dcc694ce11720f",
+    "url": "/static/js/main.7c9ea5ca.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
